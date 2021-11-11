721794784ddf82caaec3230d36c8dbbab3ecd9e1
IbrahimBagalwa <bagamurhulaibrahim@gmail.com>
2021-11-11 15:39:23 +0200

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,51 @@
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import { GlobalStyle } from './globalStyle';
-import Hero from './components/Hero';
-import Footer from './components/Footer';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 import Product from './components/Product';
 import Checkout from './components/Checkout';
 
 function App() {
+
+  const openMenu = ()=>{
+    document.querySelector('.sidebar').classList.add('open');
+  }
+  const closeMenu = ()=>{
+    document.querySelector('.sidebar').classList.remove('open')
+  }
   return (
-    <Router>
-      <GlobalStyle/>
-      <Hero/>
-      <Switch>
-        <Route exact path='/' component={Product}/>
-        <Route exact path='/checkout' component={Checkout}/> 
-      </Switch>
-      <Footer/>
-    </Router>
+    <BrowserRouter>
+    <div className="grid-container">
+      <header className="header">
+          <div className="brand">
+              <button onClick={openMenu}>
+                  &#9776;
+              </button>
+              <Link to="/">Somba</Link>
+          </div>
+          <div className="header-links">
+              <Link to='/cart'>Cart</Link>
+              <Link to="/signIn">Sign-In</Link>
+          </div>
+      </header>
+      <aside className="sidebar">
+        <h3>Shopping Categories</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+        <ul className="sidebar-links">
+            <li>
+                <a href="index.html">Pizza</a> 
+            </li>
+            <li>
+                <a href="index.html">Desserts</a> 
+              </li>
+        </ul>
+    </aside>
+
+      <main className="main">
+        <div className="content">
+            <Route exact={true} path='/' component={Product}/>
+        </div>
+      </main>
+      <footer className="footer">All right reserved.</footer>
+    </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/Products/index.js
+++ client/src/components/Products/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {useHistory} from 'react-router-dom'
 import { 
   ProductsContainer,
   ProductWrapper,
@@ -13,6 +14,7 @@ import {
  } from './ProductElement';
 
 function ProductsCheckout({heading, data}) {
+  const history = useHistory()
     return (
         <ProductsContainer id='pizza'>
         <ProductsHeading>{heading}</ProductsHeading>
@@ -25,7 +27,7 @@ function ProductsCheckout({heading, data}) {
                   <ProductTitle>{product.name}</ProductTitle>
                   <ProductDesc>{product.desc}</ProductDesc>
                   <ProductPrice>{product.price}</ProductPrice>
-                  <ProductButton>{product.button}</ProductButton>
+                  <ProductButton onClick={()=>history.push('/cardItem')}>{product.button}</ProductButton>
                 </ProductInfo>
               </ProductCard>
             );
