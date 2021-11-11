0cbabbe62d34238504093e0d2af44b6243d739df
IbrahimBagalwa <bagamurhulaibrahim@gmail.com>
2021-11-11 15:44:28 +0200

    update

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,19 +1,9 @@
 import React,{useState} from 'react'
-import Navbar from '../Nabar'
 import { HeroContainer, HeroContent, HeroItems, HeroH1, HeroBtn, HeroP } from './HeroElement';
-import Sidebar from '../Sidebar'
 
 function Index() {
-    const [isOpen, setIsOpen ] = useState(false);
-
-    const toggle = () => {
-        setIsOpen(!isOpen)
-    }
-
     return (
       <HeroContainer>
-          <Navbar toggle={toggle}/>
-          <Sidebar isOpen={isOpen} toggle={toggle}/>
           <HeroContent>
               <HeroItems>
                   <HeroH1>Greatest Food Ever</HeroH1>
--- client/src/components/Product/index.js
+++ client/src/components/Product/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Products from '../Products';
 import { productData, productDataTwo } from '../Products/data';
 import Features from '../Features';
-
+import Hero from '../Hero'
 
 function Product() {
     return (
         <>
+            <Hero/>
             <Products  heading='Choose your favorite' data={productData}/>
-            <Features/>
+                <Features/>
             <Products heading='Sweet Treats for You' data={productDataTwo}/>
         </>
     )
