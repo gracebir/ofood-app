618cd3e4cf5107c2f7ea82ecc3d93e3cff2c5a06
IbrahimBagalwa <bagamurhulaibrahim@gmail.com>
2021-11-14 13:31:17 +0200

    update

---
+++ server/routes/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import userRoutes from './client.routes.js';
+
+const router = express.Router();
+
+router
+    .use('/user', userRoutes)
+ 
+
+export default router
